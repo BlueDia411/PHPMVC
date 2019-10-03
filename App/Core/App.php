4cc8d8089a7abb1b1fd78b0b64655bb12c3434cf
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-03 16:25:14 +0700

    Fix Error pada App.php pada pengkondisian

--- App/Core/App.php
+++ App/Core/App.php
@@ -1,9 +1,17 @@
 <?php 
 
 class App {
+	protected $controller = 'Home';
+	protected $method = 'index';
+	protected $params = [];
+
 	public function __construct() {
+
 		$url = $this->parseURL();
-		var_dump($url);
+		if (file_exists('../App/Controllers/' . $url[0] . '.php')) {
+			$this->controller = $url[0];
+			unset($url[0]);
+		}
 	}
 
 	public function parseURL(){
