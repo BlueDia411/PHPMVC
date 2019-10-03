e185507e7bb3013093367d7d2508bf5bce6061d1
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-03 16:57:05 +0700

    membuat About.php

---
+++ App/Controllers/About.php
@@ -0,0 +1,9 @@
+<?php 
+class About{
+	public function index($nama = Yogi, $pekerjaan = Mahasiswa){
+		echo "Hi $nama, pekerjaan";
+	}
+	public function page(){
+		echo 'About/page';
+	}
+}
--- App/Core/App.php
+++ App/Core/App.php
@@ -8,10 +8,31 @@ class App {
 	public function __construct() {
 
 		$url = $this->parseURL();
+
+		//Controller
 		if (file_exists('../App/Controllers/' . $url[0] . '.php')) {
 			$this->controller = $url[0];
 			unset($url[0]);
 		}
+
+		require_once'../App/Controllers/' . $this->controller . '.php';
+		$this->controller = new $this->controller;
+		
+		// Method
+		if (isset($url[1])) {
+			if (method_exists($this->controller, $url[1])) {
+				$this->method = $url[1];
+				unset($url[1]);
+			}
+		}
+
+		//params
+		if (!empty($url)) {
+			$this->params = array_values($url);
+		}
+
+		//Jalankan controller & method
+		call_user_func_array([$this->controller, $this->method], $this->params);
 	}
 
 	public function parseURL(){
