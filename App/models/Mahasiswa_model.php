480b3327b7d2d8f679d42511b31ee455339a9386
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-07 11:36:27 +0700

    membuat wrapper

--- App/models/Mahasiswa_model.php
+++ App/models/Mahasiswa_model.php
@@ -1,5 +1,13 @@
 <?php 
 class Mahasiswa_model{
+
+	private $table = 'mahasiswa';
+	private $db;
+
+	public function __construct(){
+		$this->db = new Database;
+	}
+
 	public function getAllMahasiswa(){
 		$this->stmt = $this->dbh->prepare('SELECT * FROM mahasiswa');
 		$this->stmt->execute();
