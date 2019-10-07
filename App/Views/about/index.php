16937d3f3f3005d0e2c2f0b322ba7de642b03293
Yogi Nugraha <yoginugraha43@gmail.com>
2019-10-07 09:59:04 +0700

    fix error pada controller model

--- App/Views/about/index.php
+++ App/Views/about/index.php
@@ -1,5 +1,5 @@
-<div class="container mt-3">
-	<h1>About Me</h1>
+<div class="container">	
+	<h1 class="mt-3">About Me</h1>
 	<img src="<?= BASEURL; ?>/IMG/18342290_1503110313042779_671085036165006328_n.jpg" alt="Yogi Nugraha" width="200" class="rounded-circle">
-	<p>Hi, Nama saya <?php= $data['nama']; ?>, saya seorang <?php= $data['pekerjaan']; ?> berumur <?php= $data['umur'];?> tahun</p>
+	<p>Hi, Nama saya <?=$data['nama'];?>, saya seorang <?=$data['pekerjaan'];?> berumur <?=$data['umur'];?> tahun.</p>
 </div>
